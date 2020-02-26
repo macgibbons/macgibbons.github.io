a9883cb4863ba073966cbfb3eaeb7765ce4d96c6
macgibbons <macgibbons@gmail.com>
2020-02-26 14:42:00 -0600

    button hover color changed

--- js/main.js
+++ js/main.js
@@ -88,7 +88,8 @@ $(document).ready(function(){
 
 
     // Select all links with hashes
-    $('.navbar-nav a[href*="#"]')
+    $('.navbar-nav a[href*="#"]') 
+
     // Remove links that don't actually link to anything
     .not('[href="#"]')
     .not('[href="#0"]')
@@ -123,6 +124,44 @@ $(document).ready(function(){
       }
     }
     });
+
+        // Select all links with hashes
+        $('.landing-content a[href*="#"]') 
+
+        // Remove links that don't actually link to anything
+        .not('[href="#"]')
+        .not('[href="#0"]')
+        .on('click',function(event) {
+        // On-page links
+        if (
+          location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+          && 
+          location.hostname == this.hostname
+        ) {
+          // Figure out element to scroll to
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+          // Does a scroll target exist?
+          if (target.length) {
+            // Only prevent default if animation is actually gonna happen
+            event.preventDefault();
+            $('html, body').animate({
+              scrollTop: target.offset().top-50
+            }, 1000, function() {
+              // Callback after animation
+              // Must change focus!
+              var $target = $(target);
+              $target.focus();
+              if ($target.is(":focus")) { // Checking if the target was focused
+                return false;
+              } else {
+                $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+                $target.focus(); // Set focus again
+              };
+            });
+          }
+        }
+        });
 
       $(document).ready(function() {
           $('#mc_embed_signup').find('form').ajaxChimp();
