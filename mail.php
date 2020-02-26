e365d5bfcdafa7fd5007f739b5b173d47620885a
macgibbons <macgibbons@gmail.com>
2020-02-26 13:33:15 -0600

    php mail test

--- mail.php
+++ mail.php
@@ -1,14 +1,14 @@
 <?php
     $to = 'macgibbons@gmail.com';
-    $name = $_POST["name"];
-    $email= $_POST["email"];
-    $text= $_POST["message"];
+    $name = $_POST['name'];
+    $email= $_POST['email'];
+    $text= $_POST['message'];
  
     
 
 
     $headers = 'MIME-Version: 1.0' . "\r\n";
-    $headers .= "From: " . $email . "\r\n"; // Sender's E-mail
+    $headers .= 'From: ' . $email . "\r\n"; // Sender's E-mail
     $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
 
     $message ='<table style="width:100%">
@@ -21,7 +21,7 @@
         
     </table>';
 
-    mail ($to, $email, $message, $headers)
+    mail($to, $email, $message, $headers)
 
     // if (@mail($to, $email, $message, $headers))
     // {
