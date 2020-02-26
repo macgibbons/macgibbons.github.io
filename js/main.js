754b2923cf043c2b6919c560894af784ab7a1f1f
macgibbons <macgibbons@gmail.com>
2020-02-26 15:01:34 -0600

    added resume to download

--- js/main.js
+++ js/main.js
@@ -84,6 +84,61 @@ $(document).ready(function(){
     });
 
 
+    $(document).ready(function() {
+
+      $('html').addClass('js-enabled');
+  
+     
+      $(window).load(function() {
+          $(".js-preloader").fadeOut(800, function() {
+              $(".js-main-container").fadeIn(800);
+  
+              setup_scrollreveal();
+              setup_progress_bar_animation();
+          });
+      });
+  
+  });
+  
+  
+  
+  function setup_progress_bar_animation()
+  {
+      var $animation_elements = $("[class*='a-']");
+      var $window = $(window);
+  
+      $window.on('scroll resize', function() {
+          var window_height = $window.height();
+          var window_top_position = $window.scrollTop();
+          var window_bottom_position = (window_top_position + window_height);
+  
+          $.each($animation_elements, function() {
+              var $element = $(this);
+              var element_height = $element.outerHeight();
+              var element_top_position = $element.offset().top;
+              var element_bottom_position = (element_top_position + element_height);
+  
+              // Check to see if this current container is within viewport
+              if ((element_bottom_position >= window_top_position) &&
+                  (element_top_position <= window_bottom_position)) {
+                  $element.addClass('in-view');
+  
+                  // Animate progress bar
+                  if ($element.hasClass('a-progress-bar')) {
+                      $element.css('width', $element.attr('data-percent') + '%');
+                  }
+  
+              }
+              //else {
+              //    $element.removeClass('in-view');
+              //}
+          });
+      });
+  
+      $window.trigger('scroll');
+  
+  }
+  
 
 
 
